00f399206bd72a01cc41935598578eaa68f4c751
JIA-YU YI <tb9002@hotmail.com>
2017-06-15 18:52:42 +0800

    * command.h

---
+++ command.h
@@ -0,0 +1,36 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#define MAGIC_KEY        0x11111111
+#define ACK              0x20000001
+#define NACK             0x20000002
+
+#define LL_GPIO_LOW      0x31000000 //1
+#define LL_GPIO_HIGH     0x32000000 //2
+#define LL_GPIO_2        0x30000001 //3
+#define LL_GPIO_3        0x30000002 //3
+#define LL_GPIO_4        0x30000004
+#define LL_GPIO_5        0x30000008
+#define LL_GPIO_6        0x30000010
+#define LL_GPIO_7        0x30000020
+#define LL_GPIO_8        0x30000040
+#define LL_GPIO_9       0x30000080
+#define LL_GPIO_10      0x30000100
+#define LL_GPIO_11      0x30000200
+#define LL_GPIO_12      0x30000400
+#define LL_GPIO_13      0x30000800
+#define LL_GPIO_16      0x30001000
+#define LL_GPIO_17      0x30002000
+#define LL_GPIO_18      0x30004000
+#define LL_GPIO_19      0x30008000
+#define LL_GPIO_20      0x30010000
+#define LL_GPIO_21      0x30020000
+#define LL_GPIO_22      0x30040000
+#define LL_GPIO_23      0x30080000
+#define LL_GPIO_24      0x30100000
+#define LL_GPIO_25      0x30200000
+#define LL_GPIO_26      0x30400000
+#define LL_GPIO_27      0x30800000
+
+
+#endif /* end of include guard: COMMAND_H */
